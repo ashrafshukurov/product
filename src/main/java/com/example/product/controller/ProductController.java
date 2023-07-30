6145d5ae8180ec04a731301c8c18207fddc2e5fe
Ashraf <esrefsukurov7@gmail.com>
2023-07-30 08:47:24 +0400

    GetProductById is commented

--- src/main/java/com/example/product/controller/ProductController.java
+++ src/main/java/com/example/product/controller/ProductController.java
@@ -17,10 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
-   @GetMapping("/{id}")
-    public ResponseEntity<ProductResponse> getProductByID(@PathVariable Long id){
-       return ResponseEntity.ok(productService.getProductById(id));
-   }
+//   @GetMapping("/{id}")
+//    public ResponseEntity<ProductResponse> getProductByID(@PathVariable Long id){
+//       return ResponseEntity.ok(productService.getProductById(id));
+//   }
    @GetMapping("/dec/{id}/{amount}")
     public boolean decreaseCountByCount(@PathVariable Long id,int count){
       return productService.decreaseCountByCount(id,count);
